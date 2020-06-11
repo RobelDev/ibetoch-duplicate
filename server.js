e6fdf523ac19bb3907b746d6c3fda58af5f4d526
Robel Tegegne <robelkTegegne@gmail.com>
2020-06-11 05:04:58 -0700

    connecting db

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
+
+//connect to mongo database
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("APP BETOCH");
