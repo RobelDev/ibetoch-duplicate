72eed5c7bc6f7979ec22cb7f298cfc0ff52a3f8f
Robel Tegegne <robelkTegegne@gmail.com>
2020-07-08 11:09:39 -0700

    preparing for deplyoment

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const connectDB = require("./config/db");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
-//const GridFsStorage = require("multer-gridfs-storage");
-//var methodOverride = require("method-override");
+const path = require("path");
 //connect to mongo database
 connectDB();
 
@@ -18,12 +17,16 @@ app.use(express.json({ extended: false }));
 //app.use(methodOverride("_method"));
 //initialize the passport auth
 app.use(cors());
-//app.use(passport.initialize());
-//app.use(passport.session());
 
-// app.get("/", (req, res) => {
-//   res.send("APP BETOCH");
-// });
+//
+if (process.env.NODE_ENV === "production") {
+  //set static foler
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
