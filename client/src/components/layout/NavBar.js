944799c23cf8f0a49f2e5c2cea246e72a97a96f2
Robel Tegegne <robelkTegegne@gmail.com>
2020-07-31 18:06:58 -0700

    nav

--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -194,7 +194,7 @@ const NavBar = ({ history, authState: { isAuth, loading, user }, logout }) => {
           iBetoch
         </Link>
       </h2>
-      <small className="text-danger">Official: end of Aug 2020. </small>
+      <small className="text-danger">Official: end of Aug 2020 </small>
 
       <button className="navbar-toggler" type="button" onClick={onToggle}>
         {" "}
