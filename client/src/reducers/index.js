49514377afd900a2b40403e2783a9dab3fba7850
Robel Tegegne <robelkTegegne@gmail.com>
2020-10-25 09:29:56 -0700

    deleted some files

--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "./authReducer";
-import propertyReducer from "./propertyReducer";
-//add reducers here when created in reducer folder
-export default combineReducers({
-  authReducer,
-  propertyReducer,
-});
